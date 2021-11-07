cd66ae058d72e13cc9c1ef3565fa8139002c4d4f
taapp <julius.nieminen@helsinki.fi>
2021-11-07 21:10:37 +0200

    Lisätty testauskattavuuteen liittyviä tiedostoja ja testejä päivitetty kattavammiksi.

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,33 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisays_negatiivisen_lisays(self):
+        self.assertAlmostEqual(self.varasto.lisaa_varastoon(-8), None)
+
+    def test_ottaminen_negatiivisen_ottaminen(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-8), 0.0)
+    
+
+    def test_ottaminen_otetaan_enemman_kuin_saldo(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(10), 8)
+
+    def test_alkusaldo_negatiivinen(self):
+        varasto_temp = Varasto(10,-10)
+        self.assertAlmostEqual(varasto_temp.saldo, 0.0)
+
+    def test_tilavuus_negatiivinen(self):
+        varasto_temp = Varasto(-1)
+        self.assertAlmostEqual(varasto_temp.tilavuus, 0.0)
+
+    def test_lisays_enemman_kuin_tilaa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.lisaa_varastoon(8)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_str(self):
+        self.varasto.lisaa_varastoon(8)
+        self.assertAlmostEqual(str(self.varasto), f"saldo = 8, vielä tilaa 2")
+
